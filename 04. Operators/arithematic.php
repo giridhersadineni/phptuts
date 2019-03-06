ee8a103219b73b869f5b1c87f5177cd68917b63a
Giridher Sadineni <giridher@hotmail.com>
2019-03-06 12:00:03 +0530

    updated some files class midway

---
+++ 04. Operators/arithematic.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Arithematic Operators</title>
+</head>
+<body>
+<?php
+$a=10;
+$b=3;
+echo '<p>a + b =' . ($a + $b) .'</p>';
+
+echo '<p>a - b =' . ($a - $b) .'</p>';
+
+echo '<p>a * b =' . ($a * $b) .'</p>';
+
+echo '<p>a / b =' . ($a / $b) .'</p>';
+
+echo '<p>a % b =' . ($a % $b) .'</p>';
+
+echo '<p>a ˆ b =' . ($a ** $b) .'</p>';
+
+
+?>
+</body>
+</html>
--- 04. Operators/readme.md
+++ 04. Operators/readme.md
@@ -6,9 +6,13 @@
 |---|---|---|
 |-$a	|Negation	|Opposite of $a.|
 
-$a + $b	Addition	Sum of $a and $b.
+| $a + $b	|Addition	|Sum of $a and $b.|
 $a - $b	Subtraction	Difference of $a and $b.
 $a * $b	Multiplication	Product of $a and $b.
 $a / $b	Division	Quotient of $a and $b.
 $a % $b	Modulo	Remainder of $a divided by $b.
 $a ** $b	Exponentiation	Result of raising $a to the $b'th power. Introduced in PHP 5.6.
+
+
+# References Links
+['Assignment Operator Reference'](http://php.net/manual/en/language.operators.assignment.php)
